29692214d640b5b455d7364d2d55e4558f62a781
Mohit Kumar <148181778+MOHIT0525@users.noreply.github.com>
2025-07-15 11:58:11 +0530

    Add files via upload

---
+++ calc.js
@@ -0,0 +1,17 @@
+
+function dis(n){
+    console.log(`Button ${n} pressed`)
+    document.getElementById("result").value += n
+
+}
+function solve(){
+    document.getElementById("result").value
+
+}
+
+function clr() {
+    document.getElementById("result").value = ""
+}
+function dis(val) {
+			document.getElementById("result").value += val
+}
